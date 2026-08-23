fc74638cd381f9ac7064c25919d79f5985d8049e
rasberrytech <reedx33@gmail.com>
2026-08-23 08:08:59 -0700

    Connect service environment to native app

--- source/ios/ModernRBLX2016App.swift
+++ source/ios/ModernRBLX2016App.swift
@@ -2,9 +2,12 @@ import SwiftUI
 
 @main
 struct ModernRBLX2016App: App {
+    @StateObject private var environment = ServiceEnvironment()
+
     var body: some Scene {
         WindowGroup {
             RootView()
+                .environmentObject(environment)
         }
     }
 }
