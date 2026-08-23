2b5fabb9f7bc51425328cdcc9fd6e20b93c76a14
rasberrytech <reedx33@gmail.com>
2026-08-23 08:16:17 -0700

    Connect native app to service environment and eligibility gate

--- source/ios/ModernRBLX2016App.swift
+++ source/ios/ModernRBLX2016App.swift
@@ -3,11 +3,15 @@ import SwiftUI
 @main
 struct ModernRBLX2016App: App {
     @StateObject private var environment = ServiceEnvironment()
+    @StateObject private var eligibility = EligibilityController()
 
     var body: some Scene {
         WindowGroup {
-            RootView()
+            AccountGateView(eligibility: eligibility)
                 .environmentObject(environment)
+                .task {
+                    await eligibility.evaluate(verified: environment.mode == .mock ? true : nil)
+                }
         }
     }
 }
